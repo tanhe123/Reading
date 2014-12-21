1984e9c107990639b1e94ad64ef5d833e9c9860c
Hehe Tan <xiayule148@gmail.com>
2014-12-21 16:03:25 +0800

    first commit

---
+++ src/main/java/net/xiayule/reading/HomeController.java
@@ -0,0 +1,20 @@
+package net.xiayule.reading;
+
+import com.sun.glass.ui.mac.MacPasteboard;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by tan on 14-12-21.
+ */
+@Controller
+// @RequestMapping("/mvc") // 根 url 路径
+public class HomeController {
+
+    @RequestMapping({"/", "/home"})
+    public String showHomePage() {
+
+        return "home";
+    }
+
+}
---
+++ src/main/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: tan
+  Date: 14-12-21
+  Time: 下午1:42
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title></title>
+  </head>
+  <body>
+123
+  </body>
+</html>
