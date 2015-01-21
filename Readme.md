5b54f28410137a6bbccb484128697c2ccc030da7
Hehe Tan <xiayule148@gmail.com>
2015-01-21 20:46:41 +0800

    删除对于 wmd-input 的按键监视，使用 page-down 的 preConversion 事件来改变save状态

--- Readme.md
+++ Readme.md
@@ -1,5 +1,8 @@
 
 
+同步功能
+
+
 1. 字体图标使用 [font-awesome](http://fontawesome.dashgame.com/)
 2. Ztree 可以制作树形目录结构
 3. 布局可以使用 jquery ui layout
--- src/main/web/res/main.js
+++ src/main/web/res/main.js
@@ -9,10 +9,16 @@ var synchronizer = (function () {
     var syncQueue = undefined;
     synchronizer.init = function() {
         syncQueue = ";";
+
+        // Load the queue from localStorage in case a previous synchronization was aborted
         if(localStorage["sync.queue"]) {
             syncQueue = localStorage["sync.queue"];
         }
-    }
+
+        if(localStorage["sync.current"]) {
+            this.addFile(localStorage["sync.current"]);
+        }
+    };
 
     // Add a file to the synchronization queue
     synchronizer.addFile = function(fileIndex) {
@@ -20,7 +26,56 @@ var synchronizer = (function () {
             syncQueue += fileIndex + ";";
             localStorage["sync_queue"] = syncQueue;
         }
+    };
+
+/*
+    // Recursive function to run synchronization of a single file on multiple locations
+    function sync(fileSyncIndexList, content, title) {
+        if (fileSyncIndexList.length === 0) {
+            localStorage.removeItem("sync.current");
+            //todo:
+            //unsetWorkingIndicator(FLAG_SYNCHRONIZE);
+            running = false;
+            // run the next file synchronization
+            synchronizer.run();
+            return;
+        }
+        var fileSyncIndex = fileSyncIndexList.pop();
+        // Try to find the provider
     }
+*/
+
+    var running = false;
+
+    /*synchronizer.run = function() {
+        // If synchronization is already running or nothing to synchronize
+        if(running || syncQueue.length === 1) {
+            return;
+        }
+
+        // Start synchronization of the next available in the queue
+        //todo:
+        //setWorkingIndicator(FLAG_SYNCHRONIZE);
+        running = true;
+
+        // Dequeue the fileIndex
+        // 获得第二个;号的位置，第一个是开始的标志，第二个表示一个fileIndex的结束
+        var separatorPos = syncQueue.indexOf(";", 1);
+        // 截取到 fileIndex
+        var fileIndex = syncQueue.substring(1, separatorPos);
+        localStorage["sync.current"] = fileIndex;
+        // 将取出的 fileIndex 从队列中删除
+        syncQueue = syncQueue.substring(separatorPos);
+        // 保存被修改的队列
+        localStorage["sync.queue"] = syncQueue;
+
+        var content = localStorage[fileIndex + ".content"];
+        var title = localStorage[fileIndex + ".title"];
+
+        // todo: Parse the list of synchronized locations associated to the file
+        var fileSyncIndexList = localStorage[fileIndex + ".sync"].split(";");
+        sync(fileSyncIndexList, content, title);
+    };*/
 
     return synchronizer;
 })(jQuery);
@@ -39,6 +94,9 @@ var fileManager = (function($) {
         // 自动调用保存
         window.setInterval(function() {
             fileManager.saveFile();
+
+            //todo: 还未写同步功能
+            //synchronizer.run();
         }, 3000);
 
         $("#new-file").click(function() {
@@ -71,10 +129,9 @@ var fileManager = (function($) {
             save = true;
         });
 
-        $("#wmd-input").keyup(function() {
+/*        $("#wmd-input").keyup(function() {
             save = true;
-        });
-
+        });*/
     };
 
     // 新建一个文件
@@ -123,7 +180,10 @@ var fileManager = (function($) {
         var fileIndex = localStorage["file.current"];
         $("#wmd-input").val(localStorage[fileIndex + ".content"]);
 
-        core.createEditor()
+        core.createEditor(function () {
+           save = true;
+        });
+
         this.updateFileTitleUI();
     };
 
@@ -189,7 +249,8 @@ var fileManager = (function($) {
             var content = $("#wmd-input").val();
             var fileIndex = localStorage["file.current"];
             localStorage[fileIndex + ".content"] = content;
-            synchronizer.addFile(fileIndex);
+            //todo: 同步功能
+            //synchronizer.addFile(fileIndex);
             save = false;
         }
     };
@@ -289,9 +350,15 @@ var core = (function ($) {
         });
     };
 
-    core.createEditor = function() {
+    core.createEditor = function(textChangeCallback) {
         $("#wmd-button-bar").empty();
         var converter = Markdown.getSanitizingConverter();
+
+        converter.hooks.chain("preConversion", function (text) {
+            textChangeCallback();
+            return text;
+        });
+
         var editor = new Markdown.Editor(converter);
         editor.run();
 
